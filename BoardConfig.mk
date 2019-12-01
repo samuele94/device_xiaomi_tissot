a53de57b0d9dfe4c389f63b407191acffa0ca427
ProchyGaming <mcmovieslovakiasro@gmail.com>
2019-12-01 18:26:44 +0100

    Revert "tissot: Use pixel fp to bypass safetynet"
    This reverts commit 76aaa6d4899bc2c45c9c563df1f3afd259415432.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,3 @@ endif
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk
-
-# Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_tissot
-TARGET_RECOVERY_DEVICE_MODULES := libinit_tissot
