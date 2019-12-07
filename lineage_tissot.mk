90bb528f64a4359d6451e25daf587c30f55ae4db
samuele94 <samuelepetri94@gmail.com>
2019-12-07 11:25:38 +0100

    little fix

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := aosp_tissot
+PRODUCT_NAME := lineage_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
